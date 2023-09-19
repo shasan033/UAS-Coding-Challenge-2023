e83c1a74f1fb1f6bcde2f2390658dcaba14f76de
shasan01 <shasan01@students.cs.ubc.ca>
2023-09-19 16:56:41 -0700

    first commit

---
+++ gps_processor.py
@@ -0,0 +1,28 @@
+import haversine as hs
+from haversine import Unit
+import pandas as pd
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9090
+
+# Create client socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((HOST, PORT))
+gps_coordinates = clientSocket.recv(4096).decode()
+print(gps_coordinates)
+
+
+# Take server data and parse it
+df = pd.DataFrame([map(float, x.split("\t")) for x in gps_coordinates.split("\n")])
+print(df)
+
+speed = []
+for i in range(len(df) - 2):
+    loc1 = df.iloc[i]
+    loc2 = df.iloc[i + 1]
+    distance = hs.haversine(loc1, loc2, unit=Unit.METERS)
+    speed.append(distance)
+
+print(speed)
+print(sum(speed) / len(speed))
---
+++ gps_server.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9090
+
+# Get GPS data file
+with open("missionwaypoints.txt") as f:
+    gps_coordinates = f.read()
+    print(f"Parsed GPS coordinates: \n{gps_coordinates}")
+
+
+# Create websocket for server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    client, addr = s.accept()
+    with client:
+        print(f"Connected to {addr}")
+        while True:
+            client.sendall(gps_coordinates.encode("utf-8"))
